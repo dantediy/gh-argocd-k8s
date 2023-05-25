7c7c1f468faa7329f92a916cede56ebe1f0b22ba
khoatd <khoatd@SS-KHOATD.vbd.local>
2023-05-25 10:54:23 +0700

    added e_version

---
+++ server/core/constants/app-const.js
@@ -0,0 +1,20 @@
+module.exports = {
+  version: '2.46.0', // sprint number
+  vdms: {
+      aaaa: process.env.hehe || 'https://asdasdasd.asdasd.com',
+      bbb: '/aa/vdbbms/cc/dd/ff',
+      ccc: '/aa/vdbbms/cc/dd/ff',
+      ddd: '/aa/vdbbms/cc/dd/ff',
+      eee: '/aa/vdbbms/cc/dd/ff',
+      fff: '/aa/vdbbms/cc/dd/ff',
+      ggg: process.env.haha || 'ASJHDBASJDHASKD-ASJUDHBASDHB-123123#!@#ASD',
+      hhh: '/aa/vdbbms/cc/dd/ff',
+      jjj: '/aa/vdbbms/cc/dd/ff',
+      kkk: '/aa/vdbbms/cc/dd/ff',
+      lll: process.env.hihi || '/ff/aa/vdbbms/cc/dd/ff',
+  },
+  qqq: {
+      www: '/aa/vdbbms/cc/dd/ff'
+  },
+  eee: 50,
+};
